b22c26ae1d49acc9101a0698c29d8761b33c0b88
azamatcto <azamatkhabibullaevcto@gmail.com>
2024-07-05 21:27:09 +0500

    Add tailwind and shadcn/ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,71 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "@/styles/index.css";
+import { cn } from "@/lib/utils";
+
+const openSans = localFont({
+  src: [
+    {
+      path: "../assets/fonts/Open_Sans/300/OpenSans-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Open_Sans/400/OpenSans-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Open_Sans/500/OpenSans-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Open_Sans/600/OpenSans-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Open_Sans/700/OpenSans-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Open_Sans/800/OpenSans-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+  variable: "--font-open-sans",
+});
+
+const merriweather = localFont({
+  src: [
+    {
+      path: "../assets/fonts/Merriweather/300/Merriweather-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Merriweather/400/Merriweather-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Merriweather/700/Merriweather-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Merriweather/900/Merriweather-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+  variable: "--font-merriweather",
+});
 
 export const metadata: Metadata = {
   title: "",
@@ -11,8 +78,10 @@ type RootLayoutProps = {
 
 function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html className={cn(openSans.variable, merriweather.variable)} lang="en">
+      <body className="bg-background text-foreground antialiased font-sans">
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
